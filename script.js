f4dec1586462b373ce6387959bd2ad5e4010dc6f
Cwz <129849156+Q3dlaXpoaQ@users.noreply.github.com>
2023-05-02 14:50:49 +0800

    Big update
    除了index.html，其它内容全部用javascript渲染代码渲染

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function functionforlogs() {
+function rendering_forlogs() {
     window.onload = function () {
         var url = "datas.json"
         var request = new XMLHttpRequest();
@@ -19,7 +19,9 @@ function functionforlogs() {
         }
     }
 }
-function functionforrecord() {
+
+
+function rendering_forrecord() {
     window.onload = function () {
         var url = "datas.json"
         var request = new XMLHttpRequest();
@@ -32,11 +34,69 @@ function functionforrecord() {
                 let p = document.createElement("p");
                 let a = document.createElement("a");
                 p.innerHTML = json[listnumber]["thing"];
-                a.innerHTML =json[listnumber]["time"];
+                a.innerHTML = json[listnumber]["time"];
                 document.body.append(article);
                 article.insertAdjacentElement("afterbegin", p);
                 article.insertAdjacentElement("afterbegin", a);
+                console.log(listnumber)
+                if (json[listnumber]["see_more"] != null) {
+                    let now_number = listnumber;
+                    let link_a = document.createElement("a");
+                    link_a.innerHTML = "————>>";
+                    article.onmousemove = function () {
+                        document.body.style.cursor = "pointer";
+                        article.style = "margin-left: 18%;margin-right: 22%;"
+                    };
+                    article.onmouseout = function () {
+                        document.body.style.cursor = "auto";
+                        article.style = "margin-left: 20%;margin-right: 20%;"
+                    };
+                    article.insertAdjacentElement("beforeend", link_a);
+                    article.onclick = function () {
+                        window.open('record.html');
+                        localStorage.setItem("record_name", json[now_number]["see_more"]);
+                    };
+                };
             };
+        }
+    }
+}
+
+
+function rendering_forrecord_pages() {
+    var record_name = null;
+    if (sessionStorage.getItem("record_name") != null) {
+        record_name = sessionStorage.getItem('record_name');
+    }
+    else {
+        record_name =localStorage.getItem('record_name');
+        sessionStorage.setItem('record_name', record_name);
+        localStorage.clear();
+    };
+    window.onload = function () {
+        var url = "records.json"
+        var request = new XMLHttpRequest();
+        request.open("get", url);
+        request.send(null);
+        request.onload = function () {
+            var json = JSON.parse(request.responseText)[record_name][0];
+            document.title = json["title"];
+            let title = document.createElement("h1")
+            title.innerHTML = json["title"];
+            document.body.appendChild(title);
+            for (var text_number in json["text"]) {
+                let p = document.createElement("p");
+                p.innerHTML = json["text"][text_number];
+                p.id = "p" + text_number;
+                document.body.appendChild(p);
+            };
+            for (var img_number in json["img"]) {
+                let img = document.createElement("img");
+                img.src = json["img"][img_number]["img_url"];
+                console.log(json["img"][img_number]["img_url"]);
+                let pos_p = document.getElementById("p" + json["img"][img_number]["postion"]);
+                document.body.insertBefore(img, pos_p.nextSibling);
+            }
         }
     }
 }
