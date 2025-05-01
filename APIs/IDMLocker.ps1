1d0e000c92675bf521fc2145713210ee370364f3
Cwz <129849156+Q3dlaXpoaQ@users.noreply.github.com>
2025-05-01 09:47:56 +0800

    Create IDMLocker.ps1

---
+++ APIs/IDMLocker.ps1
@@ -0,0 +1,4 @@
+write-host "IDM-Activation-Script"
+write-host "https://sp.chwz.dpdns.org/https://github.com/WindowsAddict/IDM-Activation-Script"
+write-host ""
+write-host "This project has been archived and is no longer being maintained."
