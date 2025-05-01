48c3e841e941b413d8a09de078d4907de96c18a7
Cwz <129849156+Q3dlaXpoaQ@users.noreply.github.com>
2025-05-01 10:15:28 +0800

    Update and rename IDMLocker.ps1 to IDM.cmd

--- APIs/IDMLocker.ps1
+++
@@ -1,4 +0,0 @@
-write-host "IDM-Activation-Script"
-write-host "https://sp.chwz.dpdns.org/https://github.com/WindowsAddict/IDM-Activation-Script"
-write-host ""
-write-host "This project has been archived and is no longer being maintained."
